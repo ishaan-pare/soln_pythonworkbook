05473f083d36ab5e57089daf43e9fcf19ec78820
ishaan-pare <ishaan.pare2020@vitbhopal.ac.in>
2023-02-18 20:31:52 +0530

    added soln Hello

---
+++ Unit1/Hello.py
@@ -0,0 +1,10 @@
+# Exercise 2: Hello
+# (9 Lines)
+# Write a program that asks the user to enter his or her name. The program should
+# respond with a message that says hello to the user, using his or her name.
+
+
+name = str(input("Enter Your Name :"))
+
+pri = " ".join([x.capitalize() for x in name.split()])
+print(f"Hello {pri}, Welcome!!")
