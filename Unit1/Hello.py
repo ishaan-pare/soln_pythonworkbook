bc763a39ffecea2b4476455449be7e90753a74f7
ishaan-pare <ishaan.pare2020@vitbhopal.ac.in>
2023-02-19 11:26:11 +0530

    changed print message in Hello.py

--- Unit1/Hello.py
+++ Unit1/Hello.py
@@ -4,7 +4,8 @@
 # respond with a message that says hello to the user, using his or her name.
 
 
+
 name = str(input("Enter Your Name :"))
 
 pri = " ".join([x.capitalize() for x in name.split()])
-print(f"Hello {pri}, Welcome!!")
+print(f"Hello {pri}, Welcome!")
